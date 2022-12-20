b70fcfd5fa238b1f9faf9ce1afc528a09170ba96
daisuke-okada <okada.daisuke@lmi.ne.jp>
2022-12-20 16:07:14 +0900

    test: どちらも同じ数値の時の場合分けをする

--- src/tests/main.spec.ts
+++ src/tests/main.spec.ts
@@ -2,14 +2,23 @@ import { ClosedRange } from '../main'
 
 // 色々あったが共通化しないほうが良さそう
 describe('上端点と下端点が正しく設定されてる場合', () => {
-    test("下端点を返す", () => {
-        const closedRange = new ClosedRange(1, 2)
-        expect(closedRange.lowerNumber).toBe(1)
-    })
+    describe("上端点と下端点が異なる数値の場合", () => {
+        test("下端点を返す", () => {
+            const closedRange = new ClosedRange(1, 2)
+            expect(closedRange.lowerNumber).toBe(1)
+        })
 
-    test("上端点を返す", () => {
-        const closedRange = new ClosedRange(1, 2)
-        expect(closedRange.upperNumber).toBe(2)
+        test("上端点を返す", () => {
+            const closedRange = new ClosedRange(1, 2)
+            expect(closedRange.upperNumber).toBe(2)
+        })
+        })
+    describe("上端点と下端点が同じ数値の場合", () => {
+        test("どちらも3の場合上端点も下端点も3を返す", () => {
+            const closedRange = new ClosedRange(3, 3)
+            expect(closedRange.lowerNumber).toBe(3)
+            expect(closedRange.upperNumber).toBe(3)
+        })
     })
 })
 
