3ea8a33a7a3ca34069c35243923e592589af918e
daisuke-okada <okada.daisuke@lmi.ne.jp>
2022-12-20 15:56:36 +0900

    test: ツリーの変更をする

--- src/tests/main.spec.ts
+++ src/tests/main.spec.ts
@@ -1,7 +1,7 @@
 import { ClosedRange } from '../main'
 
 // 色々あったが共通化しないほうが良さそう
-describe('閉区間クラスのコンストラクタの確認をする', () => {
+describe('上端点と下端点が正しく設定されてる場合', () => {
     test("下端点を返す", () => {
         const closedRange = new ClosedRange(1, 2)
         expect(closedRange.lowerNumber).toBe(1)
@@ -14,7 +14,7 @@ describe('閉区間クラスのコンストラクタの確認をする', () => {
 })
 
 describe("上端と下端を配列形式にする場合", () => {
-    test('上端点と下端点が文字列で出力される', () => {
+    test('上端点と下端点が"[3,7]"で出力される', () => {
         const closedRange = new ClosedRange(3, 7)
         expect(closedRange.getRangeArray()).toBe("[3,7]")
     })
